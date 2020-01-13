061c6be98dd8f6b7cc9433b5b76048276425e80c
Leonardo Vincenzi <leonardo.vincenzi@studio.unibo.it>
2020-01-13 17:21:21 +0100

    Modify

--- db/query.sql
+++ db/query.sql
@@ -33,6 +33,7 @@ CREATE TABLE IF NOT EXISTS `admin` (
 CREATE TABLE IF NOT EXISTS `category` (
 	`category_id` int(11) NOT NULL AUTO_INCREMENT,
 	`category_name` varchar(50) NOT NULL,
+	`image_url` varchar(100) NOT NULL,
 	PRIMARY KEY(`category_id`)
 ) ENGINE=InnoDB;
 
