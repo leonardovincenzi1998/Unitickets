cd1367a90bd2881eb9a4451f90912ac8d38aa174
Leonardo Vincenzi <leonardo.vincenzi@studio.unibo.it>
2020-01-17 10:42:36 +0100

    Modify

--- db/database.php
+++ db/database.php
@@ -69,11 +69,27 @@ class DatabaseHelper{
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
+    public function getOrganizerNameByEvents($idevent){
+        $stmt = $this->db->prepare("SELECT organizer_name, organizer_surname FROM events,organizer WHERE organizer.organizer_id=events.organizer_id AND events.event_id=?");
+        $stmt->bind_param('i',$idevent);
+        $stmt->execute();
+        $stmt->store_result();
+        $stmt->bind_result($nome, $cognome);
+        $stmt->fetch();
+        $nome_intero= $nome.' '.$cognome;
+        return $nome_intero;
+    }
+
     public function getCategoryNameById($idcategory){
         $stmt = $this->db->prepare("SELECT category_name FROM category WHERE category_id=?");
         $stmt->bind_param('i',$idcategory);
         $stmt->execute();
-        $result = $stmt->get_result();
+        $stmt->store_result();
+        $stmt->bind_result($nome_categoria);
+        $stmt->fetch();
+        return $nome_categoria;
+
+        /*$result = $stmt->get_result();
         $output='';
 
         while($row = mysqli_fetch_array($result)) {
@@ -81,7 +97,7 @@ class DatabaseHelper{
                 <li class="breadcrumb-item active" aria-current="page">Categoria:'.$row["category_name"].'</li>
             ';
         }
-        return $output;
+        return $output;*/
     }
 
 
