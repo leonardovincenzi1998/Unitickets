8975d36756745e386ac553e686eafbb05a472866
leonardo delvecchio <leonardo.delvecchio@studio.unibo.it>
2020-01-23 12:04:31 +0100

    Create shopping-cart in modal-cart

--- html/navbar-userLogged.php
+++ html/navbar-userLogged.php
@@ -12,9 +12,47 @@
                 </div>
             </li>
             <li class="nav-item">
-                <a href="#" class="nav-link">
-                    <i class="fa fa-shopping-cart"><span id="badge-num" class="badge badge-light" style="color: #3f3f3f;">5</span></i>
-                </a>
+                <a href="#" class="nav-link" data-toggle="modal" data-target="#shoppingchartModalCenter"><i class="fa fa-shopping-cart"></i></a>
+                <div class="modal" id="shoppingchartModalCenter" tabindex="-1" role="dialog" aria-labelledby="shoppingchartModalCenterTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalCenterTitle">Carrello</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="table-responsive-sm">
+                                    <table class="table">
+                                        <thead>
+                                            <tr>
+                                                <th id="id_evento" scope="col">id</th>
+                                                <th id="event_name" scope="col">Nome dell'evento</th>
+                                                <th id="quantità_bigl" scope="col">Q.tà biglietti</th>
+                                                <th id="totale" scope="col">Totale €</th>
+                                                <th id="btn_remove" scope="col"></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <th headers="id_evento" scope="row">1</th>
+                                                <td headers="event_name">Cremonini 2C2C</td>
+                                                <td headers="quantità_bigl">1000</td>
+                                                <td headers="totale">8000000000€</td>
+                                                <td headers="btn_remove"><button class="btn btn-danger">Remove</button></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Chiudi</button>
+                                <button type="button" class="btn btn-primary">Checkout</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </li>
             <li class="nav-item dropdown">
                 <a href="#" id="navbardrop" class="nav-link dropdown-toggle" data-toggle="dropdown"><i class="fa fa-bell"></i></a>
